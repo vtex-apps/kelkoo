3528f5feb5922a16ddf3ed361c5146bd9849cf08
Ícaro <icaro.azevedo@vtex.com.br>
2020-09-29 16:57:41 -0300

    Add initial support for kelkoo integration

--- react/index.tsx
+++ react/index.tsx
@@ -4,9 +4,29 @@ import { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
-    case 'vtex:pageView': {
+    case 'vtex:orderPlaced': {
+      const { transactionId: orderId, transactionTotal: orderValue } = e.data
+
+      window._kkstrack = {
+        ...window._kkstrack,
+        orderValue: orderValue.toString(),
+        orderId,
+      }
+
+      const s = document.createElement('script')
+
+      s.type = 'text/javascript'
+      s.async = true
+      s.src = 'https://s.kk-resources.com/ks.js'
+
+      // eslint-disable-next-line prefer-destructuring
+      const x = document.getElementsByTagName('script')[0]
+
+      x.parentNode?.insertBefore(s, x)
+
       break
     }
+
     default: {
       break
     }
